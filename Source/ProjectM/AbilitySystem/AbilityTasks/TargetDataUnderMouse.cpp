b1148a1dfc8254c55370c155a6dfdec4241e5566
GianlucaBeltran <gianlucabeltran@gmail.com>
2024-03-19 18:18:48 -0600

    add projectile animation

---
+++ Source/ProjectM/AbilitySystem/AbilityTasks/TargetDataUnderMouse.cpp
@@ -0,0 +1,53 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "ProjectM/AbilitySystem/AbilityTasks/TargetDataUnderMouse.h"
+// include ability system component
+#include "AbilitySystemComponent.h"
+
+UTargetDataUnderMouse* UTargetDataUnderMouse::CreateTargetDataUnderMouse(UGameplayAbility* OwningAbility)
+{
+	UTargetDataUnderMouse* MyObj = NewAbilityTask<UTargetDataUnderMouse>(OwningAbility);
+	return MyObj;
+}
+
+void UTargetDataUnderMouse::Activate()
+{
+
+	const bool bIsLocallyControlled = Ability->GetCurrentActorInfo()->IsLocallyControlled();
+
+	if (bIsLocallyControlled)
+	{
+		SendMouseCursorData();
+	}
+	
+	
+}
+
+void UTargetDataUnderMouse::SendMouseCursorData()
+{
+	FScopedPredictionWindow ScopedPrediction(AbilitySystemComponent.Get());
+	
+	FHitResult HitResult;
+	APlayerController* PC = Ability->GetCurrentActorInfo()->PlayerController.Get();
+	if (!PC)
+	{
+		EndTask();
+		return;
+	}
+	
+	PC->GetHitResultUnderCursor(ECC_Visibility, false, HitResult);
+	// EndTask();
+
+	FGameplayAbilityTargetDataHandle DataHandle;
+	FGameplayAbilityTargetData_SingleTargetHit* Data = new FGameplayAbilityTargetData_SingleTargetHit();
+	Data->HitResult = HitResult;
+	DataHandle.Add(Data);
+	
+	AbilitySystemComponent->ServerSetReplicatedTargetData(GetAbilitySpecHandle(), GetActivationPredictionKey(), DataHandle, FGameplayTag(), AbilitySystemComponent->ScopedPredictionKey);
+
+	if (ShouldBroadcastAbilityTaskDelegates())
+	{
+		ValidData.Broadcast(DataHandle);
+	}
+}
